c0a1d2337587736edb151d791171cd1c22bb1762
Travis Wagner <trvswgnr@gmail.com>
2023-10-11 19:57:13 -0400

    add option and result classes

---
+++ test-deps.ts
@@ -0,0 +1,23 @@
+import {
+    expect,
+    describe,
+    test,
+    it,
+    beforeEach,
+    afterEach,
+    beforeAll,
+    afterAll,
+    jest,
+} from "bun:test";
+
+export {
+    expect,
+    describe,
+    test,
+    it,
+    beforeEach,
+    afterEach,
+    beforeAll,
+    afterAll,
+    jest,
+};
